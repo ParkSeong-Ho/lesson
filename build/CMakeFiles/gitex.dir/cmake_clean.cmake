4d63d266aedc415e29c036cefbb1381dd966e07c
ParkSeongHO <qkrtjdgh8040@naver.com>
2026-03-31 21:35:59 +0900

    start

---
+++ build/CMakeFiles/gitex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitex.dir/gitex.cpp.o"
+  "CMakeFiles/gitex.dir/gitex.cpp.o.d"
+  "gitex"
+  "gitex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gitex.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fmt/core.h>
+
+int main(){
+    fmt::print("안녕하세요\n");
+    fmt::print("Hello, {}!\n","wsl");
+    
+    return 0;
+}
