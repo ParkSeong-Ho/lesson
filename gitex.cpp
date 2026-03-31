da4522c2fc5cafe101c3aa8837d2ec94075dbf21
ParkSeongHO <qkrtjdgh8040@naver.com>
2026-03-31 22:14:46 +0900

    수정

--- gitex.cpp
+++ gitex.cpp
@@ -1,9 +1,9 @@
+#include <fmt/chrono.h>
 #include <iostream>
 #include <fmt/core.h>
 
 int main(){
-    fmt::print("안녕하세요\n");
-    fmt::print("Hello, {}!\n","wsl");
+    fmt::print("안녕하세요\n"); fmt::print("Hello, {}!\n","wsl");
 
     std::string s= fmt::format("The answer is {}.",42);
      std::string a= fmt::format("The answer is {}.",43);
